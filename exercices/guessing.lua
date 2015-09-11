b0656c1d9fbb6f29fa1ec5f36f9adb6d877e7f9d
Noé Falzon <falzon.noe@gmail.com>
2015-09-11 14:33:11 +0300

    Added number guessing exercise

---
+++ exercices/guessing.lua
@@ -0,0 +1,33 @@
+
+math.randomseed(os.time())
+
+local max = 1000
+local N = math.random(max)
+
+print("Welcome to the number guessing game!")
+print("I am thinking of a number between 1 and " .. max)
+
+local tries = 0
+
+while true do
+	print("Take a guess:")
+	local input = io.read()
+	local guess = tonumber(input)
+
+	if guess == nil then
+		print("Hey, " .. input .. " is not a number!")
+	else
+		tries = tries + 1
+
+		if guess < N then
+			print("Too small!")
+		elseif guess == N then
+			print("You got it, in " .. tries .. " tries!")
+			break
+		else
+			print("Too big!")
+		end
+	end
+end
+
+print("Thanks for playing!")
